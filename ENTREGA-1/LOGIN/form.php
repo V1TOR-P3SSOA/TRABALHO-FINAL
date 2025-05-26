b2de50197364aeec97439269e0c0e7b04591af94
V1TOR P <vitorpessoa36@gmail.com>
2025-05-25 21:37:58 -0300

    código atualizado

--- ENTREGA-1/LOGIN/form.php
+++ ENTREGA-1/LOGIN/form.php
@@ -9,29 +9,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login PlantCare</title>
+    <link rel="stylesheet" href="assets/estilo.css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Login</h1>
+    <div class="logo">
+        <img src="assets/estilo.css/logo.png" alt="Logo PlantCare">
+    </div>
+
+    <div id="login">
     <form action="" method="POST">
-        <label for="user_name">Nome de usuário:</label>
-        <input type="text" name="user_name" id="user_name">
-
-        <br>
-        <br>
+        <h1 id="title">Login</h1>
+        <div class="dados">
+            <label for="user_name">Nome de usuário:</label>
+            <input type="text" name="user_name" id="user_name">
+        </div>
+       
+        <div class="dados">
+            <label for="user_email">Email:</label>
+            <input type="text" name="user_email" id="user_email">
+        </div>
         
-        <label for="user_email">Email:</label>
-        <input type="text" name="user_email" id="user_email">
-
-        <br>
-        <br>
-
-        <label for="user_password">Senha:</label>
-        <input type="password" name="user_password" id="user_password">
-
-        <br>
-        <br>
-
-        <button type="submit">Entrar</button>
+        <div class="dados">
+            <label for="user_password">Senha:</label>
+            <input type="password" name="user_password" id="user_password">
+        </div>
+        
+        <div id="submit">
+            <button type="submit">Entrar</button>
+        </div>
     </form>
+    </div>
+    <img src="assets/planta.png" alt="" id="plant">
+    <div id="text">
+        <p>
+            <p class="destaque">Olá de novo! seja bem vindo</p>ao seu gerenciador <br> de plantas <br> favorito! <br> <p class="destaque2">faça login para acessar sua conta...</p>
+        </p>
+    </div>
 </body>
 </html>
