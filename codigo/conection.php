7c88ce76c0343f235206824b93391e59d59e2e9f
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-06-11 13:51:47 -0300

    mudanças momentâneas na conexão

--- codigo/conection.php
+++ codigo/conection.php
@@ -1,7 +1,7 @@
 <?php 
 
     try { 
-        $conexao = new PDO("mysql:host=localhost;dbname=plantcare;charset=utf8mb4", "root", "aluno",
+        $conexao = new PDO("mysql:host=localhost;port=3307;dbname=plantcare;charset=utf8mb4;", "root", "",
         [ 
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
