506c93cc59c01e12645d8650cf265c3cef9421b7
V1TOR P <vitorpessoa36@gmail.com>
2025-06-16 21:36:44 -0300

    link da fonte

--- codigo/login_form.php
+++ codigo/login_form.php
@@ -22,6 +22,7 @@ include_once ('login.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../assets/estilo/style_login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 
 </head>
 <body>
