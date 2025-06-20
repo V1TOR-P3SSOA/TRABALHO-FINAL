4b525312de2ec6b9c3fa66d82675abddab50958a
V1TOR P <vitorpessoa36@gmail.com>
2025-06-20 18:02:06 -0300

    Ajustes na estrutura do edit

--- codigo/edit_plant.php
+++ codigo/edit_plant.php
@@ -20,7 +20,7 @@
         if (!empty($nome) && !empty($nome_cientifico) && !empty($tipo) && !empty($ambiente) && !empty($obs)) {
             $stmt = $conexao->prepare("UPDATE plantas SET popular_name = ?, plant_type = ?, plant_environment = ?, plant_obs	 = ?, scientific_name = ? WHERE id_plant = ?");
             $stmt->execute([$nome, $nome_cientifico, $tipo, $ambiente, $obs, $id]);
-            header("Location: read_plant.php");
+            header("Location: cadastro_planta_form.php");
             exit;
         } else {
             $error = "Preencha todos os campos!";
@@ -62,40 +62,49 @@
             
     </div>
 
-    <div id="edit_form">
-        <form method="post">
-            <div class="campo">
-                <label for="nome_planta">Nome popular:</label>
-                <input type="text" id="nome_planta" name="nome_planta" value="<?= htmlspecialchars($planta['popular_name']) ?>">
+    <div id="conteudo_principal">
+        <div id="edit_form">
+            <div id="edit_titulo">
+                <h2>Editar Planta</h2>
             </div>
+            <form method="post">
+                <div class="campo">
+                    <label for="nome_planta">Nome popular:</label>
+                    <input type="text" id="nome_planta" name="nome_planta" value="<?= htmlspecialchars($planta['popular_name']) ?>">
+                </div>
 
-            <div class="campo">
-                <label for="nome_cientifico_planta">Nome científico:</label>
-                <input type="text" id="nome_cientifico_planta" name="nome_cientifico_planta" value="<?= htmlspecialchars($planta['scientific_name']) ?>">
-            </div>
+                <div class="campo">
+                    <label for="nome_cientifico_planta">Nome científico:</label>
+                    <input type="text" id="nome_cientifico_planta" name="nome_cientifico_planta" value="<?= htmlspecialchars($planta['scientific_name']) ?>">
+                </div>
 
-            <div class="campo">
-                <label for="tipo_planta">Tipo:</label>
-                <input type="text" id="tipo_planta" name="tipo_planta" value="<?= htmlspecialchars($planta['plant_type']) ?>">
-            </div>
+                <div class="campo">
+                    <label for="tipo_planta">Tipo:</label>
+                    <input type="text" id="tipo_planta" name="tipo_planta" value="<?= htmlspecialchars($planta['plant_type']) ?>">
+                </div>
 
-            <div class="campo">
-                <label for="ambiente_ideal">Ambiente ideal:</label>
-                <input type="text" id="ambiente_ideal" name="ambiente_ideal" value="<?= htmlspecialchars($planta['plant_environment']) ?>">
-            </div>
+                <div class="campo">
+                    <label for="ambiente_ideal">Ambiente ideal:</label>
+                    <input type="text" id="ambiente_ideal" name="ambiente_ideal" value="<?= htmlspecialchars($planta['plant_environment']) ?>">
+                </div>
 
-            <div class="campo">
-                <label for="cuidados_gerais:">Cuidados gerais e observações:</label>
-                <input type="text" id="cuidados_gerais" name="cuidados_gerais" value="<?= htmlspecialchars($planta['plant_obs']) ?>">
-            </div>
+                <div class="campo">
+                    <label for="cuidados_gerais:">Cuidados gerais e observações:</label>
+                    <input type="text" id="cuidados_gerais" name="cuidados_gerais" value="<?= htmlspecialchars($planta['plant_obs']) ?>">
+                </div>
+                <div id="botoes_acao">
+                    <div id="button_cancelar">
+                        <button type="button" onclick="window.location.href='cadastro_planta_form.php'">Cancelar</button>
+                    </div>
+                    <div id="button_salvar">
+                        <button type="submit">Salvar Alterações</button>
+                    </div>
+                </div>
+                
+            </form>
+        </div>
+    </div>
 
-            <div id="button_cancelar">
-                <button type="button" onclick="window.location.href='read_plant.php'">Cancelar</button>
-            </div>
-            <div id="button_salvar">
-                <button type="submit">Salvar Alterações</button>
-            </div>
-        </form>
-        
+      
 </body>
 </html>
