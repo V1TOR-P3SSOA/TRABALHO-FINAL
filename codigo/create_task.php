15e642c3bab31f54eaa21b7759a621069fe2353c
V1TOR P <vitorpessoa36@gmail.com>
2025-06-22 17:56:36 -0300

    Alteração na validação dos campo do registro de tarefas

--- codigo/create_task.php
+++ codigo/create_task.php
@@ -1,5 +1,6 @@
 <?php
-
+    ob_start();
+    session_start();
     include_once("conection.php");
 
     if (!isset($_SESSION)) {
@@ -15,25 +16,34 @@
         $prioridade_tarefa = trim($_POST['prioridade_task']);
         $data_tarefa = trim($_POST['data_criacao']);
         $status_tarefa = trim($_POST['status_task']);
+        $id_planta = trim($_POST['planta_selecionada']);
 
-        if (empty($descricao_tarefa) || empty($prioridade_tarefa) || empty($data_tarefa) || empty($status_tarefa)) {
-            $erro = "Preencha todos os campos!";
+        if (empty($descricao_tarefa) || empty($prioridade_tarefa) || empty($data_tarefa) || empty($status_tarefa) || empty($id_planta)) {
+            $_SESSION['erro'] = "Preencha todos os campos!";
+            header("Location: tarefas_form.php");
+            exit;
+        } 
+        
+        if (empty($descricao_tarefa) && empty($prioridade_tarefa) && empty($data_tarefa) && empty($status_tarefa) && empty($id_planta)) {
+            $_SESSION['erro'] = "Preencha todos os campos!";
+            header("Location: tarefas_form.php");
+            exit;
         }
 
         if (empty($erro)) {
             try {
 
-                $stmt = $conexao->prepare("INSERT INTO plantas (popular_name, scientific_name, plant_type, plant_environment, plant_obs) VALUES (:name, :scientific_name, :type, :environment, :obs)");
+                $stmt = $conexao->prepare("INSERT INTO tarefas (descricao, prioridade, data_criacao, status, plant_id) VALUES (:descricao, :prioridade, :data_criacao, :status, :plant_id)");
 
-                $stmt->bindValue(':name', $nome_popular);
-                $stmt->bindValue(':scientific_name', $nome_cientifico);
-                $stmt->bindValue(':type', $tipo);
-                $stmt->bindValue(':environment', $ambiente);
-                $stmt->bindValue(':obs', $observaçoes);
+                $stmt->bindValue(':descricao', $descricao_tarefa);
+                $stmt->bindValue(':prioridade', $prioridade_tarefa);
+                $stmt->bindValue(':data_criacao', $data_tarefa);
+                $stmt->bindValue(':status', $status_tarefa);
+                $stmt->bindValue(':plant_id', $id_planta);
 
                 if ($stmt->execute()) {
-                    $_SESSION['aviso'] = "Planta registrada com sucesso!";
-                    header('Location: cadastro_planta_form.php');
+                    $_SESSION['aviso'] = "Tarefa registrada com sucesso! O cuidado é sempre necessário...";
+                    header('Location: tarefas_form.php');
                     exit;
                 } else {
                     $aviso = "Erro ao registrar a planta.";
@@ -43,10 +53,5 @@
                 $aviso = "Erro: " . $aviso->getMessage();
             }
         }
-
-        // Se chegou até aqui, houve erro. Armazena o aviso na sessão e redireciona de volta.
-        $_SESSION['erro'] = $erro;
-        header('Location: cadastro_planta_form.php');
-        exit;
     }
 ?>
